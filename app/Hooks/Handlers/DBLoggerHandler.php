c0a9c048999f2cac5d3b9242a1d981b59711605c
Lukman Nakib <lukman.nakib@gmail.com>
2023-12-29 14:45:38 +0600

    Fix query log not showing

--- app/Hooks/Handlers/DBLoggerHandler.php
+++ app/Hooks/Handlers/DBLoggerHandler.php
@@ -15,41 +15,46 @@ class DBLoggerHandler
         if (!defined('QM_VERSION')) {
             return false;
         }
-
+        
         if (apply_filters('fql_disable_logging', false)) {
             return false;
         }
-
+        
         $settings = get_option('_fql_settings');
-
+        
         if (!$settings) {
             return false;
         }
-
+        
         if (Arr::get($settings, 'active') != 'yes' || empty($settings['modules'])) {
             return false;
         }
-
+        
         $dbq = \QM_Collectors::get('db_queries');
         $data = $dbq->get_data();
-
-        if (empty($data['dbs']['$wpdb']) || empty($data['dbs']['$wpdb']->rows)) {
+        
+        
+        if (empty($data['wpdb']) || empty($data['wpdb']->rows)) {
             return false;
         }
-
-        $rows = $data['dbs']['$wpdb']->rows;
-
+    
+        if (!Arr::get($data, 'wpdb') || empty($data['wpdb']->rows)) {
+            return false;
+        }
+        
+        $rows = $data['wpdb']->rows;
+        
         $requestData = $_POST;
         $current_url = (is_ssl() ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-
+        
         $formattedRows = [];
         foreach ($rows as $row) {
             $component = (array)$row['component'];
-
+            
             if (!in_array($component['context'], $settings['modules'])) {
                 continue;
             }
-
+            
             $formattedRow = Arr::only($row, ['caller', 'sql', 'ltime', 'type', 'result']);
             $formattedRow['context_type'] = $component['type'];
             $formattedRow['context'] = $component['context'];
@@ -59,16 +64,16 @@ class DBLoggerHandler
             if (isset($formattedRows[$formattedRow['hash']])) {
                 $formattedRow['counter'] += 1;
             }
-
+            
             $formattedRow['total_counter'] = $formattedRow['counter'];
             $formattedRow['last_url'] = $current_url;
             $formattedRow['last_data'] = $requestData;
             $formattedRows[$formattedRow['hash']] = $formattedRow;
         }
-
+        
         $this->insertRows($formattedRows);
     }
-
+    
     /**
      * @param $backTrace \QM_Backtrace
      * @return string
@@ -76,18 +81,18 @@ class DBLoggerHandler
     private function getFormattedTrace($backTrace)
     {
         $items = $backTrace->get_filtered_trace();
-
+        
         $formatted = [];
-
+        
         foreach ($items as $item) {
             if (!empty($item['calling_file']) && !empty($item['calling_line'])) {
                 $formatted[] = str_replace(ABSPATH, '', $item['calling_file']) . ' #' . $item['calling_line'] . ' => ' . $item['display'];
             }
         }
-
+        
         return implode(PHP_EOL, $formatted);
     }
-
+    
     private function insertRows($rows)
     {
         foreach ($rows as $row) {
@@ -100,7 +105,7 @@ class DBLoggerHandler
                 DbLog::create($row);
             }
         }
-
+        
         return true;
     }
 }
